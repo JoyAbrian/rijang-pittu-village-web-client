0f48294b91d16c09f22debb8de81a7bd18fbbf7e
Ruukaze <113981373+JoyAbrian@users.noreply.github.com>
2025-07-20 20:18:18 +0800

    Add landing page and Tailwind CSS setup
    Removed default App component files, added a new landing_page.jsx, and integrated Tailwind CSS by adding tailwind.config.js and updating styles. Updated dependencies to include react-router-dom and tailwindcss.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import './index.css'
-import App from './App.jsx'
+import LandingPage from './pages/landing_page'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <LandingPage />,
+  },
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <RouterProvider router={router} />
+  </StrictMode>
 )
---
+++ src/pages/landing_page.jsx
@@ -0,0 +1,9 @@
+const LandingPage = () => {
+    return (
+        <div>
+            <h1 className="text-xl">Welcome to the Landing Page</h1>
+        </div>
+    );
+}
+
+export default LandingPage
