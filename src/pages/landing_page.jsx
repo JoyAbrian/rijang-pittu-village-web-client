f4324dbf133a4092cabf5f8e054e5a36e2e549c9
joyabrianrantepasang14 <joyabrianrantepasang14@gmail.com>
2025-07-22 01:30:33 +0000

    sambutan section landing page

--- src/pages/landing_page.jsx
+++ src/pages/landing_page.jsx
@@ -1,5 +1,6 @@
 import LandingPageHero from "../components/fragments/Landing_Page/landing_page_hero";
 import LandingPageJelajahi from "../components/fragments/Landing_Page/landing_page_jelajahi";
+import LandingPageSambutan from "../components/fragments/Landing_Page/landing_page_sambutan";
 import Navbar from "../components/layouts/Navbar";
 
 const LandingPage = () => {
@@ -8,6 +9,7 @@ const LandingPage = () => {
             <Navbar />
             <LandingPageHero />
             <LandingPageJelajahi />
+            <LandingPageSambutan />
         </>
     );
 }
