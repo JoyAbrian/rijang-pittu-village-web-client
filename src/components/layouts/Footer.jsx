d866bf620e6f61547d1324073e8dc52739cee9fe
joyabrianrantepasang14 <joyabrianrantepasang14@gmail.com>
2025-07-22 09:20:55 +0000

    instagram link

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
         <footer class="bg-gray-800 text-white py-8 text-center w-full font-poppins">
             <div class="max-w-6xl mx-auto px-4">
                 <h3 class="text-2xl font-bold mb-2">Kelurahan Rijang Pittu ✅</h3>
-                <p class="text-base opacity-80">Managed by KKNT 114 Kelurahan Rijang Pittu Universitas Hasanuddin</p>
+                <p class="text-base opacity-80">Managed by <a className="underline" href="https://www.instagram.com/kknt114.rijangpittu/" target="_blank" rel="noopener noreferrer">KKNT 114 Kelurahan Rijang Pittu Universitas Hasanuddin</a></p>
             </div>
         </footer>
     );
